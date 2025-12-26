d9d5dec45fc0f6ec247e6688b9ca2af7e9d7c2fc
Arjun Dabir <arjunadabir@gmail.com>
2025-12-26 10:14:54 -0800

    added toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,29 +3,31 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import ConvexClientProvider from '@/components/convex/ConvexClientProvider';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from 'sonner';
 
 const inter = Inter({
-	variable: '--font-inter',
-	subsets: ['latin'],
+  variable: '--font-inter',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-	title: 'Calendar | Zotsites',
-	description: 'Create your UCI Quarter Course Schedule here.',
+  title: 'Calendar | Zotsites',
+  description: 'Create your UCI Quarter Course Schedule here.',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" className="overscroll-none">
-			<body className={`${inter.variable} antialiased`}>
-				<ClerkProvider>
-					<ConvexClientProvider>{children}</ConvexClientProvider>
-				</ClerkProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className="overscroll-none">
+      <body className={`${inter.variable} antialiased`}>
+        <Toaster position="bottom-center" richColors />
+        <ClerkProvider>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
