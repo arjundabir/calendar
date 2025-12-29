0e723cba69b12898305d63ce9c70372fe411cdb1
Arjun Dabir <arjunadabir@gmail.com>
2025-12-29 14:58:19 -0800

    events normalizer

---
+++ utils/calendar/calendar-normalizer.ts
@@ -0,0 +1,36 @@
+import type { Doc } from '@/convex/_generated/dataModel';
+
+type Day = 'M' | 'Tu' | 'W' | 'Th' | 'F' | 'Sa' | 'Su';
+export type NormalizedEvent = Omit<Doc<'events'>, 'event'> &
+	Omit<Doc<'events'>['event'], 'meetings'> &
+	Omit<Doc<'events'>['event']['meetings'][number], 'days'> & { dayOfWeek: Day };
+
+export function normalizeEvents(events: Doc<'events'>[]) {
+	const normalizedEvents: NormalizedEvent[] = [];
+	for (const event of events) {
+		for (const meeting of event.event.meetings) {
+			if (!meeting.timeIsTBA) {
+				const { event: unstructuredEvent, ...restOfEvent } = event;
+				const { meetings: _, ...restOfUnstructuredEvent } = unstructuredEvent;
+				const { days, ...restOfMeeting } = meeting;
+
+				const daysOfWeek = splitDays(meeting.days);
+				for (const day of daysOfWeek) {
+					normalizedEvents.push({
+						dayOfWeek: day,
+						...restOfEvent,
+						...restOfUnstructuredEvent,
+						...restOfMeeting,
+					});
+				}
+			} else {
+				// TODO: manage when the time is TBA
+			}
+		}
+	}
+	return normalizedEvents;
+}
+
+function splitDays(dayString: string): Day[] {
+	return dayString.match(/M|Tu|W|Th|F|Sa|Su/g) as Day[];
+}
